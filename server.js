014dd763485192b4b1e5fc8f3f0bf41d14301e3d
Isaac Santacruz <Isaacsantacruz73@gmail.com>
2023-06-11 21:30:20 -0400

    Setting up your index view to show your drinks data

--- server.js
+++ server.js
@@ -2,20 +2,21 @@
 
 const express = require("express")
 const drinks =require("./models/drinks")
+const { name } = require("ejs")
 
 const app = express()
 const port = 3000
 
 //routes
-app.get("/", (req, res)=>{
-    res.send("welcome to the gitpub app")
-})
+app.get("/", (req, res) => {
+    res.send("Welcome to the gitpub app");
+});
 
-app.get("/drinks", (req, res)=>{
-    res.render("index.ejs")
-})
+app.get("/drinks", (req, res) => {
+    res.render("index.ejs", { drinks: drinks });
+});
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`This ${port} port is working`);
-})
+});
 
