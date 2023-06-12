9739deeb73a26a20e1ac4ee28d04891d7c6c8855
Isaac Santacruz <Isaacsantacruz73@gmail.com>
2023-06-11 20:09:12 -0400

    third commit

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+//imports
+
 const express = require("express")
+const drinks =require("./models/drinks")
 
 const app = express()
 const port = 3000
@@ -6,6 +9,10 @@ const port = 3000
 //routes
 app.get("/", (req, res)=>{
     res.send("welcome to the gitpub app")
+})
+
+app.get("/drinks", (req, res)=>{
+    res.send(drinks)
 })
 
 app.listen(port,()=>{
